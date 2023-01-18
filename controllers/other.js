8573b8f02f1fa96b5a69d794a0ecf987bc090d72
Yusuf Boran <yusuf.boran@rbbt.com.tr>
2023-01-18 15:01:47 +0300

    socialmedia CRUD

--- controllers/other.js
+++ controllers/other.js
@@ -1,5 +1,6 @@
 const client = require("../db");
 
+//location queries
 exports.getMapList = async function (req, res) {
   try {
     var sql = `select * from locations `;
@@ -11,18 +12,15 @@ exports.getMapList = async function (req, res) {
   }
 };
 exports.createMapLocaiton = async function (req, res) {
-  const { name, adress, phone, location, image } = req.body;
+  const { name, address, phone, location, image } = req.body;
 
-  if (!(name && adress && phone && location && image)) {
+  if (!(name && address && phone && location && image)) {
     return res.status(400).send("All input is required");
   }
-
-  var lid = Date.now().toString(16).toUpperCase();
-  // console.log(new Date(parseInt("185C367C49D", 16))); 16 to 10
-
-  const values = [name, lid, adress, phone, location, image];
-
   try {
+    var lid = Date.now().toString(16).toUpperCase();
+    // console.log(new Date(parseInt("185C367C49D", 16))); 16 to 10
+    const values = [name, lid, address, phone, location, image];
     const sql =
       "INSERT INTO locations (name, lid,address,phone,location,image) VALUES ($1, $2, $3, $4, $5,$6)";
     const result = await client.query(sql, values);
@@ -46,13 +44,12 @@ exports.deleteMap = async function (req, res) {
   }
 };
 exports.updateMap = async function (req, res) {
-  const { id, name, adress, phone, location, image } = req.body;
-
-  if (!(name && adress && phone && location && image)) {
+  const { lid, name, address, phone, location, image } = req.body;
+  if (!(name && address && phone && location && image)) {
     return res.status(400).send("All input is required");
   }
   try {
-    const values = [name, adress, phone, location, image, id];
+    const values = [name, address, phone, location, image, lid];
     const sql =
       "UPDATE locations SET name=$1, address=$2, phone=$3, location=$4, image=$5 WHERE lid=$6";
     const result = await client.query(sql, values);
@@ -60,5 +57,64 @@ exports.updateMap = async function (req, res) {
   } catch (err) {
     console.log(err.stack);
     res.status(500).send("Error occurred while creating location");
+  }
+};
+
+// social media queries
+exports.getSocialMediaList = async function (req, res) {
+  try {
+    var sql = `select * from socialmedialist `;
+    const result = await client.query(sql);
+    res.status(200).send(result.rows);
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("Error occurred while creating social media event");
+  }
+};
+exports.createSocialMedia = async function (req, res) {
+  const { socialmedia, username } = req.body;
+
+  if (!(socialmedia && username)) {
+    return res.status(400).send("All input is required");
+  }
+
+  try {
+
+    const values = [socialmedia, username];
+    const sql = "INSERT INTO socialmedialist (socialmedia, username) VALUES ($1, $2)";
+    const result = await client.query(sql, values);
+    res.status(200).send("successfully created location");
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("Error occurred while creating social media event");
+  }
+};
+exports.deleteSocialMedia = async function (req, res) {
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).send("All input is required");
+  }
+  try {
+    await client.query(`DELETE from socialmedialist where id ='${id}'`);
+    res.status(200).send("successfully deleteSocialMedia");
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("Error occurred while creating social media event");
+  }
+};
+exports.updateSocialMedia = async function (req, res) {
+  const { id, socialmedia, username } = req.body;
+
+  if (!(socialmedia && username && id)) {
+    return res.status(400).send("All input is required");
+  }
+  try {
+    const values = [socialmedia, username, id];
+    const sql = "UPDATE socialmedialist SET socialmedia=$1, username=$2 WHERE id=$3";
+    const result = await client.query(sql, values);
+    res.status(200).send("successfully created location");
+  } catch (err) {
+    console.log(err.stack);
+    res.status(500).send("Error occurred while creating social media event");
   }
 };
--- routes/other.js
+++ routes/other.js
@@ -5,15 +5,24 @@ const {
   createMapLocaiton,
   deleteMap,
   updateMap,
+  
+  getSocialMediaList,
+  createSocialMedia,
+  deleteSocialMedia,
+  updateSocialMedia,
 } = require("../controllers/other.js");
 
 const router = express.Router();
-
+//location queries
 router.get("/map", getMapList);
 router.post("/map", createMapLocaiton);
 router.delete("/map", deleteMap);
 router.put("/map", updateMap);
 
-
+//social media queries
+router.get("/socialmedia", getSocialMediaList);
+router.post("/socialmedia", createSocialMedia);
+router.delete("/socialmedia", deleteSocialMedia);
+router.put("/socialmedia", updateSocialMedia);
 
 module.exports = router;
